3f5f18151f81d5a2b549f9a7da345a6123069db0
betalantz <betalantz@gmail.com>
2022-11-23 15:17:32 -0800

    add bonus deliverable

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
+function makeEmojis(minutes){
+  const emoji = minutes < 30 ? '☕️' : '🍱'
+  const interval = minutes < 30 ? 5 : 10
+  let numEmojis = Math.ceil(minutes/interval)
+  let emojiArr = emoji.repeat(numEmojis)
+  // while (numEmojis > 0){
+  //   emojiArr += emoji
+  //   numEmojis -= 1
+  // }
+  return emojiArr
+}
+
 export default function Article({title, preview, minutes, date="January 1, 1970"}) {
   return (
     <article>
       <h3>{title}</h3>
-      <small>{date}</small>
+      <small>{date} {makeEmojis(minutes)} {minutes} minute read</small>
       <p>{preview}</p>
     </article>
   )
